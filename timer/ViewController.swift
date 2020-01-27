791ae6dec2b80cec6e67ac6dbb6d0d3d6c2f67b6
avula koti <koteshpatel6@gmail.com>
2020-01-27 21:57:09 +0530

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by avula koti on 27/01/20.
+//  Copyright © 2020 avula koti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
